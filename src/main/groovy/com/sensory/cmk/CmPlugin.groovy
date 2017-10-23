d5849df53ee1d670cf06333da8c0c2bc51095270
Mark Sweeney <msweeney@sensoryinc.com>
2017-10-23 15:26:51 -0600

    added android archive and jar tasks

--- src/main/groovy/com/sensory/cmk/CmPlugin.groovy
+++ src/main/groovy/com/sensory/cmk/CmPlugin.groovy
@@ -1,5 +1,8 @@
 package com.sensory.cmk
 
+import org.gradle.api.Action
+import org.gradle.api.Plugin
+import org.gradle.api.Project
 import org.gradle.api.DefaultTask
 import org.gradle.model.Defaults
 import org.gradle.model.Each
@@ -7,6 +10,8 @@ import org.gradle.model.Finalize
 import org.gradle.model.ModelMap
 import org.gradle.model.RuleSource
 import org.gradle.api.Task
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.bundling.Zip
 
 import com.sensory.cmk.tasks.*
 import org.gradle.platform.base.BinaryTasks
@@ -14,134 +19,176 @@ import org.gradle.platform.base.ComponentBinaries
 import org.gradle.platform.base.ComponentType
 import org.gradle.platform.base.TypeBuilder
 
-class CmPlugin extends RuleSource {
-    @ComponentType
-    void registerComponent(TypeBuilder<CmakeComponent> builder) {
+class CmPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project target) {
+        File androidBuildDir = new File("${target.buildDir}/platform/android")
+
+        Task stageAndroid = target.tasks.create('stageAndroidLib', Copy.class){
+            from "${target.rootDir}/platforms/android/aar"
+            into  new File("${androidBuildDir}/aar")
+        }
+
+
+        Task jarAndroid = target.tasks.create('androidJar', Copy.class) {
+            from ("${androidBuildDir}/dist/lib/java") {
+                include "*.jar"
+                rename (/(.*)\.jar/, 'classes.jar')
+            }
+
+            from ("${androidBuildDir}/dist/lib/java") {
+                into "jni"
+                include "**/*.so"
+            }
+
+            into "${androidBuildDir}/aar"
+        }
+        jarAndroid.dependsOn(stageAndroid)
+
+        Task archiveAndroid = target.tasks.create('androidArchive', Zip.class) {
+            from new File("${androidBuildDir}/aar")
+            archiveName "${target.name}.aar"
+            destinationDir new File("${androidBuildDir}/dist")
+
+        }
+        archiveAndroid.dependsOn(jarAndroid)
+
     }
 
-    @ComponentType
-    void registerBinary(TypeBuilder<CmakeBinary> builder) {
-    }
-
-    @Defaults
-    void setDefaultLibraries(ModelMap<CmakeComponent> libraries) {
-        libraries.create('android') {
-            os = 'android'
-            arches = ['arm','arm64','x86','x86_64']
+    static class Rules extends RuleSource {
+        @ComponentType
+        void registerComponent(TypeBuilder<CmakeComponent> builder) {
         }
-        libraries.create('semiios') {
-            os = 'semiios'
-            arches = ['armv7','armv7s','arm64','x86_64','i386']
-            generator = 'Unix Makefiles'
-        }
-        libraries.create('semitizen') {
-            os = 'tizen'
-            arches = ['armv7l','x86']
-        }
-        libraries.create('seminative') {
-            os = 'seminative'
-            arches = ['x86_64']
-        }
-        libraries.create('semilinux') {
-            os = 'linux'
-            arches = ['aarch64','armv7l']
-        }
-    }
 
-    @Defaults
-    void setLibraryDefaults(@Each CmakeComponent library) {
-
-        library.generator = 'Ninja'
-        library.cmListsPath = '../../'
-        library.toolchainFile = ''
-        library.flags = ''
-    }
-
-    @Defaults
-    void setDefaults(@Each CmakeBinary cmakeBinary) {
-        cmakeBinary.generator = 'Ninja'
-        cmakeBinary.cmListsPath = '../../'
-        cmakeBinary.toolchainFile = ''
-        cmakeBinary.flags = ''
-    }
-
-    @Defaults
-    void createRootOsBuildTasks(ModelMap<Task> tasks, ModelMap<CmakeComponent> libraries) {
-        libraries.keySet().each { name ->
-            tasks.create("${name}Build", DefaultTask)
+        @ComponentType
+        void registerBinary(TypeBuilder<CmakeBinary> builder) {
         }
-    }
 
-    @ComponentBinaries
-    void createBinariesForBinaryComponent(ModelMap<CmakeBinary> binaries, CmakeComponent library) {
-        library.arches.each { architecture->
-            binaries.create(architecture) {
-                arch = architecture;
-                os = library.os
-                generator = library.generator
-                toolchainFile = library.toolchainFile ?: ''
+        @Defaults
+        void setDefaultLibraries(ModelMap<CmakeComponent> libraries) {
+            libraries.create('android') {
+                os = 'android'
+                arches = ['arm','arm64','x86','x86_64']
+            }
+            libraries.create('semiios') {
+                os = 'semiios'
+                arches = ['armv7','armv7s','arm64','x86_64','i386']
+                generator = 'Unix Makefiles'
+            }
+            libraries.create('semitizen') {
+                os = 'tizen'
+                arches = ['armv7l','x86']
+            }
+            libraries.create('seminative') {
+                os = 'seminative'
+                arches = ['x86_64']
+            }
+            libraries.create('semilinux') {
+                os = 'linux'
+                arches = ['aarch64','armv7l']
             }
         }
-    }
 
-    @BinaryTasks
-    void createCmakeTasks(ModelMap<Task> tasks, CmakeBinary binary) {
+        @Defaults
+        void setLibraryDefaults(@Each CmakeComponent library) {
 
-        String makr = binary.tasks.taskName("mkdirs", "build")
-        String genr = binary.tasks.taskName("generate", "build")
-        String runr = binary.tasks.taskName("run", "build")
-        String inst = binary.tasks.taskName("install", "build")
-        String rootOsTask = "${binary.os}Build"
-
-        tasks.create(makr,PrebuildTask){
-            it.operatingSystem = binary.os;
-            it.architecture = binary.arch;
+            library.generator = 'Ninja'
+            library.cmListsPath = '../../'
+            library.toolchainFile = ''
+            library.flags = ''
         }
-        tasks.create(genr, CmGenerateTask){
-            it.operatingSystem = binary.os;
-            it.architecture = binary.arch;
-            it.generator    = binary.generator;
-            it.cmListsPath  = "${it.project.rootDir}"
-            it.toolchainFile = binary.toolchainFile.length() > 0 ? "${it.project.rootDir}/${binary.toolchainFile}" : ''
 
-            if (it.project.ext.has('cmakeFlags')) {
-                it.flags = it.project.ext.get('cmakeFlags')
+        @Defaults
+        void setDefaults(@Each CmakeBinary cmakeBinary) {
+            cmakeBinary.generator = 'Ninja'
+            cmakeBinary.cmListsPath = '../../'
+            cmakeBinary.toolchainFile = ''
+            cmakeBinary.flags = ''
+        }
+
+        @Defaults
+        void createRootOsBuildTasks(ModelMap<Task> tasks, ModelMap<CmakeComponent> libraries) {
+            libraries.keySet().each { name ->
+                tasks.create("${name}Build", DefaultTask)
+            }
+        }
+
+        @ComponentBinaries
+        void createBinariesForBinaryComponent(ModelMap<CmakeBinary> binaries, CmakeComponent library) {
+            library.arches.each { architecture->
+                binaries.create(architecture) {
+                    arch = architecture;
+                    os = library.os
+                    generator = library.generator
+                    toolchainFile = library.toolchainFile ?: ''
+                }
+            }
+        }
+
+        @BinaryTasks
+        void createCmakeTasks(ModelMap<Task> tasks, CmakeBinary binary) {
+
+            String makr = binary.tasks.taskName("mkdirs", "build")
+            String genr = binary.tasks.taskName("generate", "build")
+            String runr = binary.tasks.taskName("run", "build")
+            String inst = binary.tasks.taskName("install", "build")
+            String rootOsTask = "${binary.os}Build"
+
+            tasks.create(makr,PrebuildTask){
+                it.operatingSystem = binary.os;
+                it.architecture = binary.arch;
+            }
+            tasks.create(genr, CmGenerateTask){
+                it.operatingSystem = binary.os;
+                it.architecture = binary.arch;
+                it.generator    = binary.generator;
+                it.cmListsPath  = "${it.project.rootDir}"
+                it.toolchainFile = binary.toolchainFile.length() > 0 ? "${it.project.rootDir}/${binary.toolchainFile}" : ''
+
+                if (it.project.ext.has('cmakeFlags')) {
+                    it.flags = it.project.ext.get('cmakeFlags')
+                } else {
+                    it.flags = ''
+                }
+                it.flags += ' ' + binary.flags;
+
+            }
+            tasks.create(runr, CmBuildTask){
+                it.operatingSystem = binary.os;
+                it.architecture = binary.arch;
+            }
+            if (binary.generator=='Ninja') {
+                tasks.create(inst, NinjaInstallTask){
+                    it.operatingSystem = binary.os;
+                    it.architecture = binary.arch;
+                }
             } else {
-                it.flags = ''
+                tasks.create(inst, MakeInstallTask){
+                    it.operatingSystem = binary.os;
+                    it.architecture = binary.arch;
+                }
             }
-            it.flags += ' ' + binary.flags;
+
+            tasks.get(genr).dependsOn(makr)
+            tasks.get(genr).dependsOn('setup')
+            tasks.get(runr).dependsOn(genr)
+            tasks.get(inst).dependsOn(runr)
 
         }
-        tasks.create(runr, CmBuildTask){
-            it.operatingSystem = binary.os;
-            it.architecture = binary.arch;
-        }
-        if (binary.generator=='Ninja') {
-            tasks.create(inst, NinjaInstallTask){
-                it.operatingSystem = binary.os;
-                it.architecture = binary.arch;
-            }
-        } else {
-            tasks.create(inst, MakeInstallTask){
-                it.operatingSystem = binary.os;
-                it.architecture = binary.arch;
-            }
-        }
 
-        tasks.get(genr).dependsOn(makr)
-        tasks.get(genr).dependsOn('setup')
-        tasks.get(runr).dependsOn(genr)
-        tasks.get(inst).dependsOn(runr)
+        @Finalize
+        void setRootOsBuildTasks(ModelMap<Task> tasks, ModelMap<CmakeComponent> libraries) {
+            libraries.each { library ->
+                library.arches.each { arch ->
+                    String installTaskName = "install"+library.name.capitalize()+arch.capitalize()+"Build"
 
-    }
+                    tasks.get("${library.os}Build").dependsOn(installTaskName)
+                }
 
-    @Finalize
-    void setRootOsBuildTasks(ModelMap<Task> tasks, ModelMap<CmakeComponent> libraries) {
-        libraries.each { library ->
-            library.arches.each { arch ->
-                String installTaskName = "install"+library.name.capitalize()+arch.capitalize()+"Build"
-
-                tasks.get("${library.os}Build").dependsOn(installTaskName)
+                if (library.os.equals('android')) {
+                    String androidArchive = 'androidArchive'
+                    tasks.get("${library.os}Build").finalizedBy(androidArchive)
+                }
             }
         }
     }
